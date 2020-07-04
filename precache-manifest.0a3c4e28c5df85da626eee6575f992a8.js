44db7a38075e45e98736cb135f30eef0d92e9d79
Yagnesh <yagnesh16697@gmail.com>
2020-07-04 11:34:01 +0530

    Updates

---
+++ precache-manifest.0a3c4e28c5df85da626eee6575f992a8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c582c0ee353e62bd131471d3a9c23d08",
+    "url": "/yagnesh16697.github.io/breaking-bad/index.html"
+  },
+  {
+    "revision": "0487e7cd86356976a78a",
+    "url": "/yagnesh16697.github.io/breaking-bad/static/css/main.71c485e1.chunk.css"
+  },
+  {
+    "revision": "edf7241344ed355d1121",
+    "url": "/yagnesh16697.github.io/breaking-bad/static/js/2.a17eb287.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yagnesh16697.github.io/breaking-bad/static/js/2.a17eb287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0487e7cd86356976a78a",
+    "url": "/yagnesh16697.github.io/breaking-bad/static/js/main.c7f0df4a.chunk.js"
+  },
+  {
+    "revision": "325011bf90a2e80eabd6",
+    "url": "/yagnesh16697.github.io/breaking-bad/static/js/runtime-main.5885bf66.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/yagnesh16697.github.io/breaking-bad/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/yagnesh16697.github.io/breaking-bad/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/yagnesh16697.github.io/breaking-bad/static/media/spinner.235b9b3e.gif"
+  }
+]);
