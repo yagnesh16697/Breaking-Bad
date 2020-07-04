580ed2a3ed3b30d48fd6490cb09288607e49bae3
Yagnesh <yagnesh16697@gmail.com>
2020-07-04 11:19:21 +0530

    First commit

---
+++ src/components/ui/Search.js
@@ -0,0 +1,25 @@
+import React,{ useState } from 'react'
+
+const Search = ( { getQuery } ) => {
+    const [text,setText] = useState('')
+    const onChange = (q) => {
+        setText(q)
+        getQuery(q)
+    }
+
+    return (
+        <section>
+            <form>
+                <input 
+                    type='text' 
+                    className='form-control'
+                    placeholder="Search characters"
+                    autoFocus
+                    onChange={(e)=>onChange(e.target.value)}
+                />
+            </form>
+        </section>
+    )
+}
+
+export default Search
